bd6df1e7ed26eff96d4d63b7d3164faca6cb1126
aminfranko <aminfranko@gmail.com>
2021-12-16 21:19:03 +0330

    Added Category
    Added Categorys to project

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
 # Create your models here.
 class Post(models.Model):
     image = models.ImageField(upload_to='blog/',default='blog/default.jpg')
@@ -9,7 +16,7 @@ class Post(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
     # tag
-    # category
+    category = models.ManyToManyField(Category)
     counted_view = models.IntegerField(default=0)
     status = models.BooleanField(default=False)
     published_date = models.DateTimeField(null=True)
@@ -23,4 +30,6 @@ class Post(models.Model):
 
     def __str__(self):
         return "{} - {}".format(self.title, self.id)
+
+    
     
