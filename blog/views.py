cd8b369364104b466a0303f0964790ae08f145f3
aminfranko <aminfranko@gmail.com>
2021-12-16 18:08:46 +0330

    edited blog
    edited blog page and inserted URL s

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,4 @@
-# from django.http import request
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from blog.models import Post
 
 # Create your views here.
@@ -8,11 +7,13 @@ def blog_view(request):
     context = {'posts' : posts}
     return render(request, 'blog/blog-home.html', context)
 
-def blog_single(request):
-    return render(request, 'blog/blog-single.html')
+def blog_single(request,pid):
+    post = get_object_or_404(Post, pk=pid)
+    context = {'post':post}
+    return render(request, 'blog/blog-single.html',context)
 
-def test(request):
-    posts = Post.objects.all()
-    # posts = Post.objects.filter(status = 0)
-    context = {'posts' : posts}
+def test(request, pid):
+    # posts = Post.objects.get(id=pid)
+    post = get_object_or_404(Post, pk=pid)
+    context = {'post':post}
     return render(request, 'test.html', context)
