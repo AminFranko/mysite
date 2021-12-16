718dc0997f514577c901f96c2cada809d2a9c3f3
aminfranko <aminfranko@gmail.com>
2021-12-16 11:30:57 +0330

    Fetched data in test file
    Fetched data in test html file

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.http import request
+# from django.http import request
 from django.shortcuts import render
+from blog.models import Post
 
 # Create your views here.
 def blog_view(request):
@@ -7,3 +8,9 @@ def blog_view(request):
 
 def blog_single(request):
     return render(request, 'blog/blog-single.html')
+
+def test(request):
+    posts = Post.objects.all()
+    # posts = Post.objects.filter(status = 0)
+    context = {'posts' : posts}
+    return render(request, 'test.html', context)
