04f404cc002ae471bb2b700df07818df4a7d6615
aminfranko <aminfranko@gmail.com>
2021-12-22 16:08:41 +0330

    filtered posts by authors
    filtered posts in blog by authors and then used kwargs

--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,12 @@ from django.shortcuts import render, get_object_or_404
 from blog.models import Post
 
 # Create your views here.
-def blog_view(request):
+def blog_view(request,**kwargs):
     posts = Post.objects.filter(status = 1)
+    if kwargs.get('cat_name') != None:
+        posts = posts.filter(category__name = kwargs['cat_name'])
+    if kwargs.get('author_username') != None:
+        posts = posts.filter(author__username = kwargs['author_username'])
     context = {'posts' : posts}
     return render(request, 'blog/blog-home.html', context)
 
@@ -22,3 +26,9 @@ def test(request, pid):
 
 def test(request):
     return render(request, 'test.html')
+
+def blog_category(request,cat_name):
+    posts = Post.objects.filter(status = 1)
+    posts = Post.objects.filter(category__name = cat_name)
+    context = {'posts':posts}
+    return render(request, 'blog/blog-home.html', context)
