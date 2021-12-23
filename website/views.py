c6d9ab5af3063a3059d15a64daa0b41d0d67d4dd
aminfranko <aminfranko@gmail.com>
2021-12-23 21:41:06 +0330

    Added messages
    Added messages in contact page for submitted or not submitted ticket

--- website/views.py
+++ website/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
 from website.models import Contact
 from website.forms import NameForm,ContactForm,NewsletterForm
+from django.contrib import messages
 
 def index_view(request):
     return render(request, 'website/index.html')
@@ -16,6 +17,10 @@ def contact_view(request):
         form = ContactForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.add_message(request,messages.SUCCESS,'your ticket submitted successfully')
+        else:
+            messages.add_message(request,messages.ERROR,'your ticket did not submitted')
+
     form = ContactForm()
     return render(request, 'website/contact.html') 
 
