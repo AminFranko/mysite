3c17d6d52b19d82d1031c1db69b7d662a31dd89f
aminfranko <aminfranko@gmail.com>
2021-12-23 19:18:10 +0330

    created test form
    created test form and ready for Model Form

--- website/views.py
+++ website/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
+from website.models import Contact
+from website.forms import NameForm
 
 def index_view(request):
     return render(request, 'website/index.html')
@@ -12,3 +14,22 @@ def contact_view(request):
 
 def elements_view(request):
     return render(request, 'website/elements.html') 
+
+
+
+def test_view(request):
+    if request.method =='POST':
+        form = NameForm(request.POST)
+        if form.is_valid():
+            name =form.cleaned_data['name']
+            email =form.cleaned_data['email']
+            subject =form.cleaned_data['subject']
+            message =form.cleaned_data['message']
+            print(name,email,subject,message)
+            return HttpResponse('done')
+        else:
+            return HttpResponse('not valid')
+
+    form = NameForm()
+    return render(request,'test.html',{'form':form})
+    
