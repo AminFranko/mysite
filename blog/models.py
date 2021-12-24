3f6da0353e3dbcee93e41161c08ed1d97c0f9923
aminfranko <aminfranko@gmail.com>
2021-12-24 12:06:05 +0330

    added site map
    added site map to project
    
    http://127.0.0.1:8000/sitemap.xml

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
@@ -33,6 +34,9 @@ class Post(models.Model):
 
     def snippets(self):
         return self.content[:150] +'. . .'
+
+    def get_absolute_url(self):
+        return reverse('blog:single',kwargs={'pid':self.id})
 
     
     
